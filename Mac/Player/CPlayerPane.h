2d9aec01f30afa01586f235b075ce7daadd8a6c4
Eric Kidd <eric.m.kidd@dartmouth.edu>
2002-02-19 10:20:23 +0000

    This commit was manufactured by cvs2svn to create branch 'old-win32-3.1.1-Stabilization'.

--- Mac/Player/CPlayerPane.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CPlayerPane : public LPane {
-	
-	public:
-		void InitPlayerPane(const SPaneInfo	&inPaneInfo);
-		void SPaneInfo CreatePaneInfo(LView *superView, Rect inRect);
-};
