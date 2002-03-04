71a11158b9bba2db037d674f6940144b888e1bc3
Elizabeth Hamon <hamon@iml.dartmouth.edu>
2002-03-04 15:30:50 +0000

    Moved OS specific configuration to TPlatform.h
    Changes by Elizabeth and Eric, okayed by Eric.
    
    
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@27119 b24aca02-f418-0410-9c49-835e4aa915c3

---
+++ Common/TPlatform.h
@@ -0,0 +1,115 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//   (c) Copyright 1999, Trustees of Dartmouth College, All rights reserved.
+//        Interactive Media Lab, Dartmouth Medical School
+//
+//			$Author$
+//          $Date$
+//          $Revision$
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#if !defined (_TPlatform_h_)
+#define _TPlatform_h_
+
+
+/*-----------------------------------------------------------------
+
+OVERVIEW
+	Platform-specific configuration code.
+
+TPLATFORM MACRO DEFINITIONS
+	#define ASSERT(x)
+
+AUTHOR
+    Chuck Officer<br>
+
+-----------------------------------------------------------------*/
+
+
+//=========================================================================
+//  QuickTime Configuration
+//=========================================================================
+
+#if defined (__QTML__) || defined (macintosh)
+#define _5L_QUICKTIME_
+#endif
+
+
+//=========================================================================
+//  Win32 Configuration
+//=========================================================================
+
+#if defined (WIN32)
+#define _5L_WIN32_
+
+#if defined (_DEBUG)
+#define DEBUG
+#endif
+
+#define ASSERT(x) _ASSERTE(x)
+
+// For now, the Windows engine uses some non-standard string functions
+// instead of using our own, equivalent code.
+#define HAVE__STRLWR 1
+#define HAVE__STRUPR 1
+#define HAVE__STRICMP 1
+
+
+//=========================================================================
+//  Macintosh Configuration
+//=========================================================================
+
+#elif defined (macintosh)
+
+#define WANT_FIVEL_NAMESPACE (1)
+
+#define NEWLINE_CHAR	'\r'
+#define RETURN_CHAR		'\n'
+
+#ifdef Debug_Throw
+#define DEBUG
+
+#define ASSERT(expr) assert(expr)
+
+// backwards compatible defines
+//#define DEBUG_5L
+//#define DEBUG_5L_SCRIPT
+
+#else
+
+#define ASSERT(x)
+
+#endif	// Debug_Throw
+
+
+//=========================================================================
+//  Other Platform Configuration
+//=========================================================================
+
+#else
+
+#define ASSERT(expr) assert(expr)
+
+#endif
+
+
+//=========================================================================
+//  Namespace Configuration
+//=========================================================================
+//  It's possible to define most of our common classes in a
+//  'FiveL' namespace.
+
+#ifdef WANT_FIVEL_NAMESPACE
+#	define BEGIN_NAMESPACE_FIVEL  namespace FiveL {
+#	define END_NAMESPACE_FIVEL    }
+#	define USING_NAMESPACE_FIVEL  using namespace FiveL;
+#	define FIVEL_NS				  FiveL::
+#else
+#	define BEGIN_NAMESPACE_FIVEL
+#	define END_NAMESPACE_FIVEL
+#	define USING_NAMESPACE_FIVEL
+#	define FIVEL_NS
+#endif
+
+#endif // _TPlatform_h_
