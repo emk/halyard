01747a6504270cd94c8f5b1fcd31d009d522691e
Zev Lowe <zeb@iml.dartmouth.edu>
2002-07-15 16:39:55 +0000

    3.3.13 - 15 July 2002 - zeb, emk   * Language change: (IF cond true_cmd false_cmd) now takes arbitrary     expressions for 'cond'.  The following new primitives have     been added: AND, OR, NOT, contains, =, <>, <, >, <=, >=.   * Added a new (LOG filename msg) command, which allows the programmer     to write to "5L", "debug" and "MissingMedia" logs.   * Major logging improvements: All primitives are now automatically     logged in a standard format (bug #1003).   * Adjusting of coordinates using origin is now logged.   * Callbacks are now logged in a much more useful fashion.   * Old arithmetic primitives now return a value (add, sub, div).   * Added MakeQuotedString to TTemplateUtils and wrote a matching test suite.
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@27399 b24aca02-f418-0410-9c49-835e4aa915c3

---
+++ Common/TTemplateUtilsTests.cpp
@@ -0,0 +1,36 @@
+// -*- Mode: C++; tab-width: 4; -*-
+
+#include <string.h>
+
+#include "TTemplateUtils.h"
+#include "ImlUnit.h"
+
+USING_NAMESPACE_FIVEL
+
+extern void test_TTemplateUtils (void);
+
+struct QuotedStringTestData
+{
+	char *input;
+	char *output;
+};
+
+QuotedStringTestData quoted_string_tests[] = {
+	{"", "\"\""},
+	{"\\", "\"\\\\\""},
+	{"foo\"bar\"", "\"foo\\\"bar\\\"\""},
+	{"'", "\"'\""},
+	{NULL, NULL}
+};
+
+
+void test_TTemplateUtils (void)
+{
+    TEST(MakeStringLowercase("aBCd") == std::string("abcd"));
+
+	for (QuotedStringTestData *test = &quoted_string_tests[0];
+		 test->input != NULL; test++)
+	{
+		TEST(MakeQuotedString(test->input) == std::string(test->output));
+	}
+}
