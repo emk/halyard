d0113bd34dfe8ae7832a315a8a862503c6882f78
Eric Kidd <eric.m.kidd@dartmouth.edu>
2003-06-18 07:54:28 +0000

    * New TestCase classes based on xUnit.  These run within the engine.
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@27920 b24aca02-f418-0410-9c49-835e4aa915c3

---
+++ Common/TestCase.cpp
@@ -0,0 +1,364 @@
+// -*- Mode: C++; tab-width: 4; c-basic-offset: 4; -*-
+
+#include "CommonHeaders.h"
+
+#include <iostream>
+#include <sstream>
+
+#include "TestCase.h"
+
+// Static variables definitions.
+TestRegistry *TestRegistry::sGlobalRegistry = NULL;
+
+
+//=========================================================================
+//  TestCase Methods
+//=========================================================================
+
+void TestCase::Run() {
+	SetUp();
+	try {
+		Test();
+	} catch (...) {
+		TearDown();
+		throw;
+	}
+	TearDown();
+}
+
+
+//=========================================================================
+//  TestCaseFactory Methods
+//=========================================================================
+
+TestCaseFactory::TestCaseFactory(const char *inName, TestRegistry *inRegistry)
+	: mName(inName)
+{
+	inRegistry->RegisterTestCaseFactory(this);
+}
+
+
+//=========================================================================
+//  TestCaseReport Methods
+//=========================================================================
+
+TestCaseReport::TestCaseReport(TestRunReport *inReport,
+							   std::string inName, TestResult inTestResult,
+							   const std::string &inErrorMessage,
+							   const std::string &inErrorFile,
+							   int inErrorLine)
+	: mName(inName), mTestResult(inTestResult),
+	  mErrorMessage(inErrorMessage), mErrorFile(inErrorFile),
+	  mErrorLine(inErrorLine)
+{
+	inReport->AddTestCaseReport(TestCaseReport::ptr(this));
+}
+
+
+//=========================================================================
+//  TestRunReport Methods
+//=========================================================================
+
+void TestRunReport::AddTestCaseReport(TestCaseReport::ptr inTestCaseReport)
+{
+	mResultCount[inTestCaseReport->GetTestResult()]++;
+	mTestCaseReports.push_back(inTestCaseReport);
+}
+
+std::string TestRunReport::GetSummary()
+{
+	std::ostringstream out;
+	if (AnyTestFailed())
+		out << "FAIL: " << GetNumTestsFailed() << " failed, ";
+	else
+		out << "OK: ";
+	out << GetNumTestsPassed() << " passed, "
+		<< GetNumTestsSkipped() << " skipped";
+	return out.str();
+}
+
+
+//=========================================================================
+//  TestRegistry Methods
+//=========================================================================
+
+TestRegistry *TestRegistry::GetGlobalRegistry()
+{
+	if (sGlobalRegistry == NULL)
+		sGlobalRegistry = new TestRegistry;
+	return sGlobalRegistry;
+}
+
+void TestRegistry::RegisterTestCaseFactory(TestCaseFactory *inFactory) {
+	mTestCaseFactories.push_back(inFactory);
+}
+
+TestRunReport::ptr TestRegistry::RunAllTests(ITestProgressMeter *inMeter) {
+	TestRunReport::ptr report(new TestRunReport);
+	int index = 0;
+	for (iterator i = begin(); i != end(); ++i) {
+		std::string name = (*i)->GetName();
+		try {
+			(*i)->Create()->Run();
+			new TestCaseReport(report.get(), name, TEST_PASSED);
+		} catch (TException &e) {
+			new TestCaseReport(report.get(), name, TEST_FAILED,
+							   e.GetErrorMessage(),
+							   e.GetErrorFile(), e.GetErrorLine());
+		} catch (std::exception &e) {
+			new TestCaseReport(report.get(), name, TEST_FAILED,
+							   e.what());
+		} catch (...) {
+			new TestCaseReport(report.get(), name, TEST_FAILED,
+							   "unknown exception (maybe segfault?)");
+		}
+		if (inMeter)
+			inMeter->UpdateTestProgress(index++, end() - begin(),
+										**(report->end() - 1));
+	}
+	return report;
+}
+
+
+//=========================================================================
+//  Tests
+//=========================================================================
+
+#if BUILD_TEST_CASES
+
+
+//-------------------------------------------------------------------------
+//  TestCase Tests
+//-------------------------------------------------------------------------
+
+class BootstrapTestCase : public TestCase {
+public:
+	bool mTestPassed;
+	BootstrapTestCase() { mTestPassed = false; }
+};
+
+template <class BTC>
+class TestBootstrapTestCase : public TestCase {
+	void Test() {
+		// We need to use assertions, because we haven't yet proved that
+		// the usual failure mechanism is working at this point in the
+		// bootstrap process.
+		BTC test;
+		ASSERT(!test.mTestPassed);
+		test.Run();
+		ASSERT(test.mTestPassed);
+	}
+};
+
+#define TEST_BOOTSTRAP_TEST_CASE(NAME) \
+    class Test##NAME : public TestBootstrapTestCase<NAME> {}; \
+    REGISTER_TEST_CASE(Test##NAME)
+
+template <class FTC, class ExpectedException>
+class TestFailingTestCase : public TestCase {
+public:
+	virtual std::string WantMsg() = 0;
+	void Test() {
+		// We need to use assertions, because we haven't yet proved that
+		// the usual failure mechanism is working at this point in the
+		// bootstrap process.
+		FTC test;
+		bool caught_exception = false;
+		try {
+			test.Run();
+		} catch (ExpectedException &failure) {
+			caught_exception = true;
+			ASSERT(failure.GetErrorMessage() == WantMsg());
+		}
+		ASSERT(caught_exception);
+	}
+};
+
+#define TEST_FAILING_TEST_CASE(NAME, EXCEPTION, WANT_MSG) \
+    class Test##NAME \
+		: public TestFailingTestCase<NAME, EXCEPTION> { \
+			std::string WantMsg() { return WANT_MSG; } \
+	}; \
+    REGISTER_TEST_CASE(Test##NAME)
+
+// *** Make sure tests actually get run.
+BEGIN_UNREGISTERED_TEST_CASE(WasRunTest, BootstrapTestCase) {
+	mTestPassed = true;
+} END_UNREGISTERED_TEST_CASE(WasRunTest);
+TEST_BOOTSTRAP_TEST_CASE(WasRunTest);
+
+// *** Make sure FAIL_TEST throws an exception.
+BEGIN_UNREGISTERED_TEST_CASE(FailThrowsExceptionTest, TestCase) {
+	FAIL_TEST("my message");
+} END_UNREGISTERED_TEST_CASE(WasRunTest);
+TEST_FAILING_TEST_CASE(FailThrowsExceptionTest, TestFailed, "my message");
+
+// *** Make sure CHECK_THROWN blocks exceptions.
+BEGIN_UNREGISTERED_TEST_CASE(PassCheckThrownTest, BootstrapTestCase) {
+	CHECK_THROWN(TException, THROW("Random exception"));
+	mTestPassed = true;
+} END_UNREGISTERED_TEST_CASE(PassCheckThrownTest);
+TEST_BOOTSTRAP_TEST_CASE(PassCheckThrownTest);
+
+// *** Make sure CHECK_THROWN fails if no exception occurs.
+BEGIN_UNREGISTERED_TEST_CASE(FailCheckThrownTest, TestCase) {
+	CHECK_THROWN(TestFailed, 2 + 2);
+} END_UNREGISTERED_TEST_CASE(FailCheckThrownTest);
+TEST_FAILING_TEST_CASE(FailCheckThrownTest, TestFailed,
+					   "Expected an exception: 2 + 2");
+
+// *** Make sure CHECK_THROWN fails if the wrong exception occurs.
+BEGIN_UNREGISTERED_TEST_CASE(CheckThrownChecksTypeTest, TestCase) {
+	CHECK_THROWN(TestFailed, THROW("unexpected exception type"));
+} END_UNREGISTERED_TEST_CASE(CheckThrownChecksTypeTest);
+TEST_FAILING_TEST_CASE(CheckThrownChecksTypeTest, TException,
+					   "unexpected exception type");
+
+BEGIN_UNREGISTERED_TEST_CASE(PassCheckEqualTest, BootstrapTestCase) {
+	CHECK_EQ(2, 1 + 1);
+	mTestPassed = true;
+} END_UNREGISTERED_TEST_CASE(PassCheckEqualTest);
+TEST_BOOTSTRAP_TEST_CASE(PassCheckEqualTest);
+
+BEGIN_UNREGISTERED_TEST_CASE(FailCheckEqualTest, BootstrapTestCase) {
+	CHECK_EQ(1, 1 + 1);
+} END_UNREGISTERED_TEST_CASE(FailCheckEqualTest);
+TEST_FAILING_TEST_CASE(FailCheckEqualTest, TestFailed,
+					   "expected 1 == 1 + 1, got: 1, 2");
+
+BEGIN_UNREGISTERED_TEST_CASE(FailStringCheckEqualTest, BootstrapTestCase) {
+	CHECK_EQ(std::string("foo"), "bar");
+} END_UNREGISTERED_TEST_CASE(FailStringCheckEqualTest);
+TEST_FAILING_TEST_CASE(FailStringCheckEqualTest, TestFailed,
+					   "expected std::string(\"foo\") == \"bar\", "
+					   "got: foo, bar");
+
+BEGIN_UNREGISTERED_TEST_CASE(PassCheckNotEqualTest, BootstrapTestCase) {
+	CHECK_NE(1, 1 + 1);
+	mTestPassed = true;
+} END_UNREGISTERED_TEST_CASE(PassCheckNotEqualTest);
+TEST_BOOTSTRAP_TEST_CASE(PassCheckNotEqualTest);
+
+BEGIN_UNREGISTERED_TEST_CASE(FailCheckNotEqualTest, BootstrapTestCase) {
+	CHECK_NE(2, 1 + 1);
+} END_UNREGISTERED_TEST_CASE(FailCheckNotEqualTest);
+TEST_FAILING_TEST_CASE(FailCheckNotEqualTest, TestFailed,
+					   "expected 2 != 1 + 1, got: 2, 2");
+
+class WasSetUpTest : public BootstrapTestCase {
+public:
+	void SetUp()    { mTestPassed = true; };
+	void Test()     { CHECK_EQ(mTestPassed, true); }
+};
+TEST_BOOTSTRAP_TEST_CASE(WasSetUpTest);
+
+class TearDownTestCase : public BootstrapTestCase {
+public:
+	void TearDown() { mTestPassed = true; }	
+};
+
+BEGIN_UNREGISTERED_TEST_CASE(WasTornDownTest, TearDownTestCase) {
+	CHECK_EQ(mTestPassed, false);
+} END_UNREGISTERED_TEST_CASE(WasTornDownTest);
+TEST_BOOTSTRAP_TEST_CASE(WasTornDownTest);
+
+BEGIN_UNREGISTERED_TEST_CASE(WasTornDownAfterFailingTest, TearDownTestCase) {
+	FAIL_TEST("test failed");
+} END_UNREGISTERED_TEST_CASE(WasTornDownAfterFailingTest);
+BEGIN_TEST_CASE(TestWasTornDownAfterFailingTest, TestCase) {
+	WasTornDownAfterFailingTest test;
+	CHECK_THROWN(TestFailed, test.Run());
+	CHECK_EQ(test.mTestPassed, true);
+} END_TEST_CASE(TestWasTornDownAfterFailingTest);
+
+
+//-------------------------------------------------------------------------
+//  TestFactory/TestRegistry/Test*Report Tests
+//-------------------------------------------------------------------------
+
+BEGIN_UNREGISTERED_TEST_CASE(SimplePassingTestCase, TestCase) {
+	CHECK_EQ(2, 2);
+} END_UNREGISTERED_TEST_CASE(SimplePassingTestCase);
+
+BEGIN_UNREGISTERED_TEST_CASE(SimpleFailingTestCase, TestCase) {
+	CHECK_EQ(1, 1 + 1);
+} END_UNREGISTERED_TEST_CASE(SimpleFailingTestCase);
+
+BEGIN_TEST_CASE(TestRegistryRegisterTest, TestCase) {
+	TestRegistry registry;
+	CHECK_EQ(registry.begin(), registry.end());
+	TestCaseFactoryImpl<SimplePassingTestCase>
+		created_factory("SimplePassingTestCase", &registry);
+    CHECK_NE(registry.begin(), registry.end());
+	TestCaseFactory *registered_factory = *registry.begin();
+	CHECK_EQ(&created_factory, registered_factory);
+	CHECK_EQ(registered_factory->GetName(), "SimplePassingTestCase");
+	boost::shared_ptr<TestCase> test(registered_factory->Create());
+	test->Run();
+} END_TEST_CASE(TestRegistryRegisterTest);
+
+BEGIN_TEST_CASE(TestRegistryRunAllTest, TestCase) {
+	TestRegistry registry;
+	TestCaseFactoryImpl<SimplePassingTestCase>
+		factory1("SimplePassingTestCase", &registry);
+	TestCaseFactoryImpl<SimpleFailingTestCase>
+		factory2("SimpleFailingTestCase", &registry);
+	TestRunReport::ptr report = registry.RunAllTests();
+	CHECK_EQ(report->GetNumTestsPassed(), 1);
+	CHECK_EQ(report->GetNumTestsFailed(), 1);
+	CHECK_EQ(report->GetNumTestsSkipped(), 0);
+	CHECK_EQ(report->GetSummary(), "FAIL: 1 failed, 1 passed, 0 skipped"); 
+	TestRunReport::iterator i = report->begin();
+	CHECK_EQ((*i)->GetTestResult(), TEST_PASSED);
+	CHECK_EQ((*i++)->GetName(), "SimplePassingTestCase");
+	CHECK_EQ((*i)->GetTestResult(), TEST_FAILED);
+	CHECK_EQ((*i)->GetName(), "SimpleFailingTestCase");
+	CHECK_EQ((*i++)->GetErrorMessage(), "expected 1 == 1 + 1, got: 1, 2");
+	CHECK_EQ(i, report->end());
+} END_TEST_CASE(TestRegistryRunAllTest);
+
+BEGIN_TEST_CASE(TestRegistryRunAllOk, TestCase) {
+	TestRegistry registry;
+	TestCaseFactoryImpl<SimplePassingTestCase>
+		factory1("SimplePassingTestCase", &registry);
+	TestRunReport::ptr report = registry.RunAllTests();
+	CHECK_EQ(report->GetSummary(), "OK: 1 passed, 0 skipped"); 
+} END_TEST_CASE(TestRegistryRunAllOk);
+
+class DummyTestProgressMeter : public ITestProgressMeter {
+	int mNextTestIndex;
+
+public:
+	DummyTestProgressMeter() : mNextTestIndex(0) {}
+
+	int GetTestsSeenCount() { return mNextTestIndex; }
+
+	void UpdateTestProgress(int inTestIndex, int inTestCount,
+							const TestCaseReport &inReport)
+	{
+		ASSERT(0 <= inTestIndex && inTestIndex < inTestCount);
+		CHECK_EQ(inTestIndex, mNextTestIndex++);
+		CHECK_EQ(inTestCount, 3);
+		if (inTestIndex == 1)
+			CHECK_EQ(inReport.GetTestResult(),
+					 TEST_FAILED);
+		else
+			CHECK_EQ(inReport.GetTestResult(),
+					 TEST_PASSED);
+	}
+};
+
+BEGIN_TEST_CASE(TestRegistryProgressMeterTest, TestCase) {
+	TestRegistry registry;
+	TestCaseFactoryImpl<SimplePassingTestCase>
+		factory1("SimplePassingTestCase", &registry);
+	TestCaseFactoryImpl<SimpleFailingTestCase>
+		factory2("SimpleFailingTestCase", &registry);
+	TestCaseFactoryImpl<SimplePassingTestCase>
+		factory3("SimplePassingTestCase", &registry);
+	DummyTestProgressMeter meter;
+	registry.RunAllTests(&meter);
+	CHECK_EQ(meter.GetTestsSeenCount(), 3);
+} END_TEST_CASE(TestRegistryProgressMeterTest);
+
+#endif // BUILD_TEST_CASES
---
+++ Common/TestCase.h
@@ -0,0 +1,320 @@
+// -*- Mode: C++; tab-width: 4; c-basic-offset: 4; -*-
+
+//========================================================================
+//  Testing Framework
+//=========================================================================
+//  This testing framework is based on the xUnit family of testing
+//  frameworks, as originally designed by the eXtreme Programming
+//  community.  For a nice example of xUnit, see _Test-Driven_Development_
+//  by Kent Beck, which shows the framework in action, and how to create
+//  it in a new language.  I've followed his approach loosely here.
+
+#ifndef TestCase_H
+#define TestCase_H
+
+// Forward declarations of a few classes.
+class TestRegistry;
+class TestRunReport;
+
+//////////
+// Define BUILD_TEST_CASES if it wasn't set in the project options or
+// on the command line.  You should wrap all TestCase subclasses in
+// BUILD_TEST_CASES.
+//
+#ifndef BUILD_TEST_CASES
+#	define BUILD_TEST_CASES 1
+#endif // ndef BUILD_TEST_CASES
+
+//////////
+// A test case can have one of three results: TEST_PASSED, TEST_FAILED
+// or TEST_SKIPPED.  Note that we don't distinguish between different
+// types of failure.  Several other xUnit-style frameworks *do* make
+// this distinction, but combining all the failure types into a single
+// code seems to make the implementation simpler without the loss
+// of any useful output.
+//
+enum TestResult {
+	TEST_PASSED,
+	TEST_FAILED,
+	TEST_SKIPPED,
+	TEST_RESULT_TYPE_COUNT
+};
+
+//////////
+// If a test case fails explicity (by calling FAIL, or violating a CHECK_*
+// assertion), we throw a TestFailed exception.
+//
+class TestFailed : public TException
+{
+	std::string mMessage;
+
+public:
+	TestFailed(const char *inErrorFile, int inErrorLine,
+			   const std::string &inMessage)
+	    : TException(inErrorFile, inErrorLine, inMessage.c_str()) {}
+};
+
+//////////
+// TestCase is the heart of the framework.  It has three overridable methods:
+// SetUp, Test and TearDown.  If a family of test cases share similar
+// initialization and cleanup, then put the initialization into SetUp, and
+// the cleanup code into TearDown.  Then, for each test relying on those
+// resources, create a subclass of TestCase and override Test.  It's best
+// to use the BEGIN_TEST_CASE and END_TEST_CASE macros; they also take
+// care of properly registering your test case.
+//
+// This is the closest C++ adaptation I can achieve of the standard
+// xUnit TestCase class.
+//
+class TestCase
+{
+public:
+	void Run();
+	virtual void SetUp() {}
+	virtual void Test() = 0;
+	virtual void TearDown() {}
+};
+
+//////////
+// TestCaseFactory knows how to manufacture instances of a given TestCase
+// at runtime.  It's used to help automatically register TestCases
+// with the global TestRegistry.  The process is managed by
+// REGISTER_TEST_CASE.
+//
+class TestCaseFactory {
+	std::string mName;
+
+public:
+	TestCaseFactory(const char *inName, TestRegistry *inRegistry);
+	virtual ~TestCaseFactory() {}
+
+	std::string GetName() { return mName; }
+	virtual boost::shared_ptr<TestCase> Create() = 0;
+};
+
+//////////
+// TestCaseFactoryImpl provides an implementation of TestCaseFactory
+// for a specific subclass of TestCase.  Again, this is used
+// primarily by REGISTER_TEST_CASE.
+//
+template <class TestCaseType>
+class TestCaseFactoryImpl : public TestCaseFactory
+{
+public:
+	TestCaseFactoryImpl(const char *inName, TestRegistry *inRegistry)
+		: TestCaseFactory(inName, inRegistry) { }
+	boost::shared_ptr<TestCase> Create() { return new TestCaseType(); }
+};
+
+//////////
+// This object holds a report on the results of running a single test
+// case.
+//
+class TestCaseReport {
+public:
+	typedef boost::shared_ptr<TestCaseReport> ptr;
+
+private:
+	std::string mName;
+	TestResult mTestResult;
+
+	std::string mErrorMessage;
+	std::string mErrorFile;
+	int mErrorLine;
+
+public:
+	// This object must be created with 'new', and it immediately becomes
+	// the property of inReport.
+	// XXX - Wrap constructor in factory function.
+	TestCaseReport(TestRunReport *inReport,
+				   std::string inName, TestResult inTestResult,
+				   const std::string &inErrorMessage = "",
+				   const std::string &inErrorFile = "unknown",
+				   int inErrorLine = 0);
+
+	std::string GetName() const { return mName; }
+	TestResult GetTestResult() const { return mTestResult; }
+
+	std::string GetErrorMessage() const
+		{ ASSERT(mErrorMessage != ""); return mErrorMessage; }
+	std::string GetErrorFile() const { return mErrorFile; }
+	int GetErrorLine() const { return mErrorLine; }
+};
+
+//////////
+// Information about a complete run of zero or more TestCase objects.
+// This is basically a collection of TestCaseReport objects with
+// some summary statistics.
+//
+class TestRunReport
+{
+	typedef std::vector<TestCaseReport::ptr> TestCaseReportVector;
+
+	TestCaseReportVector mTestCaseReports;
+	std::vector<int> mResultCount;
+
+public:
+	typedef TestCaseReportVector::iterator iterator;
+	typedef boost::shared_ptr<TestRunReport> TestRunReport::ptr;
+
+	TestRunReport() : mResultCount(TEST_RESULT_TYPE_COUNT, 0) {}
+
+	void AddTestCaseReport(TestCaseReport::ptr inTestCaseReport);
+
+	iterator begin() { return mTestCaseReports.begin(); }
+	iterator end() { return mTestCaseReports.end(); }
+
+	int GetTestResultCount(TestResult inType)
+		{ return mResultCount[inType]; }
+
+	int GetNumTestsPassed() { return GetTestResultCount(TEST_PASSED); }
+	int GetNumTestsFailed() { return GetTestResultCount(TEST_FAILED); }
+	int GetNumTestsSkipped() { return GetTestResultCount(TEST_SKIPPED); }
+
+	bool AnyTestFailed() { return GetNumTestsFailed() != 0; }
+	
+	std::string GetSummary();
+};
+
+//////////
+// An abstract interface for code which reports on the status of a test
+// run.  We don't implement this here; it's the responsibility of
+// various test drivers.
+//
+class ITestProgressMeter {
+public:
+	//////////
+	// Override this method to report status.
+	//
+	// [in] inTestIndex - The zero-based index of the test just run.
+	// [in] inTestCount - The total number of tests to run.
+	// [in] inReport    - The results of running the test.
+	//
+	virtual void UpdateTestProgress(int inTestIndex, int inTestCount,
+									const TestCaseReport &inReport) = 0;
+};
+
+//////////
+// TestRegistry knows about a set of test cases, and can run them
+// on behalf of various test drivers.
+//
+// The function GetGlobalRegistry returns the registry used by
+// REGISTER_TEST_CASE.  This is the only registry which you will
+// normally need to use.
+//
+class TestRegistry
+{
+	// The REGISTER_TEST_CASE macro uses this global registry for
+	// registering test cases.
+	static TestRegistry *sGlobalRegistry;
+
+	// We're not responsible for deleting these pointers--they typically
+	// point to global variables.
+	std::vector<TestCaseFactory*> mTestCaseFactories;
+
+public:
+	static TestRegistry *GetGlobalRegistry();
+
+	typedef std::vector<TestCaseFactory*>::iterator iterator;
+
+	void RegisterTestCaseFactory(TestCaseFactory *inFactory);
+
+	iterator begin() { return mTestCaseFactories.begin(); }
+	iterator end() { return mTestCaseFactories.end(); }
+
+	TestRunReport::ptr RunAllTests(ITestProgressMeter *inMeter = NULL);
+};
+
+//////////
+// Immediately fail the current test case.
+//
+#define FAIL_TEST(MESSAGE) \
+    throw TestFailed(__FILE__, __LINE__, MESSAGE)
+
+//////////
+// Make sure that CODE throws an exception of TYPE.  If not, fail.
+//
+#define CHECK_THROWN(TYPE, CODE) \
+    do { \
+        bool caught_exception = false; \
+        try { \
+            CODE; \
+        } catch (TYPE &) { \
+            caught_exception = true; \
+        } \
+        if (!caught_exception) \
+            FAIL_TEST("Expected an exception: " ## #CODE); \
+    } while (0)
+
+//////////
+// Create a function NAME to help test OP.  OP must be a binary operator,
+// and its arguments must support operator<<.  This is macro, because
+// MSVC doesn't like overloaded operators as template parameters.  It
+// generates a template because most operators are overloaded for
+// multiple types, and we need to choose the right type.
+//
+#define DEFINE_CHECK_OP_HELPER(NAME, OP) \
+    template <class Type1, class Type2> \
+	void NAME(const char *inErrorFile, int inErrorLine, \
+			  const char *inExpr1, const char *inExpr2, \
+			  const Type1 &inVal1, const Type2 &inVal2) { \
+		if (!(inVal1 OP inVal2)) { \
+			std::ostringstream out; \
+			out << "expected " << inExpr1 << " " << #OP << " " << inExpr2 \
+				<< ", got: " << inVal1 << ", " << inVal2; \
+			throw TestFailed(inErrorFile, inErrorLine, out.str()); \
+		} \
+	}
+
+//////////
+// Fail if !(expr1 == expr2).  Both expressions must return a type
+// supporting operator<<.
+//
+#define CHECK_EQ(expr1, expr2) \
+    CheckEqualHelper(__FILE__, __LINE__, #expr1, #expr2, expr1, expr2)
+DEFINE_CHECK_OP_HELPER(CheckEqualHelper, ==)
+
+//////////
+// Fail if !(expr1 != expr2).  Both expressions must return a type
+// supporting operator<<.
+//
+#define CHECK_NE(expr1, expr2) \
+    CheckNotEqualHelper(__FILE__, __LINE__, #expr1, #expr2, expr1, expr2)
+DEFINE_CHECK_OP_HELPER(CheckNotEqualHelper, !=)
+
+//////////
+// A handy wrapper unregistered test cases; it saves typing.  You normally
+// want BEGIN_TEST_CASE and END_TEST_CASE below.
+//
+// This macro is split into two parts because we want the body of
+// TestCase::Test to be outside the macro expansion.  Although it would
+// save typing to make TestCase::Test a BODY parameter, this causes
+// the MSVC debugger to get terribly confused about line numbers, and
+// assign the same line number to all the code in the test suite.
+//
+#define BEGIN_UNREGISTERED_TEST_CASE(NAME, SUPERCLASS) \
+    class NAME : public SUPERCLASS { public: void Test()
+#define END_UNREGISTERED_TEST_CASE(NAME) }
+
+//////////
+// Register the TestCase subclass NAME with the global TestRegistry.  This
+// relies on creating a global object and letting the constructor do all
+// the work (this is a standard C++ technique for initializing code before
+// main() gets called).
+//
+#define REGISTER_TEST_CASE(NAME) \
+    TestCaseFactoryImpl<NAME> \
+        NAME##_factory(#NAME, TestRegistry::GetGlobalRegistry())
+
+//////////
+// Handy wrapper for defining a TestCase subclass and registering it with
+// the global TestRegistry.  See BEGIN_UNREGISTERED_TEST_CASE for an
+// explanation of why this macro is split into two parts.
+//
+#define BEGIN_TEST_CASE(NAME, SUPERCLASS) \
+    BEGIN_UNREGISTERED_TEST_CASE(NAME, SUPERCLASS)
+#define END_TEST_CASE(NAME) \
+    END_UNREGISTERED_TEST_CASE(NAME); \
+	REGISTER_TEST_CASE(NAME)
+
+#endif // TestCase_H
