c663e52170a3da5f8306068ebd86b25be4ac8da4
Eric Kidd <eric.m.kidd@dartmouth.edu>
2004-02-16 20:40:26 +0000

    * Added tamale-tags.sh script for Emacs users.
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@28037 b24aca02-f418-0410-9c49-835e4aa915c3

---
+++ tools/tamale-tags.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Generates Emacs TAGS files for quickly finding definitions of things.
+#
+# To use this script on Windows, you need to install Cygwin, add the Emacs
+# 'bin' directory to your path, and cd to the top-level folder of the
+# Tamale program you wish to generate tags for.
+
+REGEX='(\(def[^ \t]*\|card\|group\|sequence\|element\)[ \t]+(?\([^ \t]+\)/\2'
+etags --language=none \
+  --ignore-case-regex="/$REGEX/" Scripts/start.ss \
+  --ignore-case-regex="/  $REGEX/" \
+  `find Runtime/5L -name \*.ss` \
+  `find Scripts \! \( -name start.ss -prune \) -a -name \*.ss`
