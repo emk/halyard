1ade30aa8253f0a1bf263ac51b5fd6025b9c093a
Eric Kidd <eric.m.kidd@dartmouth.edu>
2006-09-21 00:25:41 +0000

    0.2.2 - 20 Sep 2006 - emk, brian
      * #2930: Implemented a "preinstaller" which can be used as an
        autorun.exe.  The preinstaller walks the user through installing
        QuickTime, which is helpful, since the latest version of the QuickTime
        installer is both (a) asynchronous once launched and (b) legally
        unmodifiable under the standard distribution agreement.  For now, the
        "preinstaller" is hard-coded for use with our latest project, but it
        should be easy to parameterize for other projects.
      * #3467: Applied a band-aid to the crash-handling logic so that it will
        generally restore the system task bar for all but the worst crashes.
        We could benefit from more work in this area, but a more complete fix
        would destabilize the engine.
      * #3466: Send crashes to a different server by default.
      * #3461: Disable screen saver a bit more thoroughly in full-screen mode,
        even if the user has a password protected screen saver turned on.
      * The update installer now correctly detects unwritable directories.
    
    
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@28531 b24aca02-f418-0410-9c49-835e4aa915c3

--- Common/TVersion.h
+++ Common/TVersion.h
@@ -22,14 +22,14 @@
 
 #define VERSION_MAJOR_NUM          0
 #define VERSION_MINOR_NUM          2
-#define VERSION_REV_BIG            1
+#define VERSION_REV_BIG            2
 #define VERSION_REV_SMALL          0
 
 // For various unfortunate preprocessor reasons, we need a duplicate copy
 // in string format.
 #define VERSION_MAJOR_NUM_STRING   "0"
 #define VERSION_MINOR_NUM_STRING   "2"
-#define VERSION_REV_BIG_STRING     "1"
+#define VERSION_REV_BIG_STRING     "2"
 #define VERSION_REV_SMALL_STRING   "0"
 
 #define SHORT_NAME          "Tamale"
