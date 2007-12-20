018cf5e240c1ad5647e54a4f17f06ecb6858b219
Eric Kidd <eric.m.kidd@dartmouth.edu>
2007-12-20 20:11:00 +0000

    0.4.7 - 20 Dec 2007 - emk, brian
      * F#1145: We added support for clipping around movies and other
        heavyweight elements during Stage redraws, and during transitions.
      * F#940: Turn on wxCLIP_CHILDREN for StageBackground so we don't 
        redraw the background color before redrawing the screen, which 
        causes black flashes in full screen mode.
    
    
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@28781 b24aca02-f418-0410-9c49-835e4aa915c3

--- Common/TVersion.h
+++ Common/TVersion.h
@@ -22,14 +22,14 @@
 
 #define VERSION_MAJOR_NUM          0
 #define VERSION_MINOR_NUM          4
-#define VERSION_REV_BIG            6
+#define VERSION_REV_BIG            7
 #define VERSION_REV_SMALL          0
 
 // For various unfortunate preprocessor reasons, we need a duplicate copy
 // in string format.
 #define VERSION_MAJOR_NUM_STRING   "0"
 #define VERSION_MINOR_NUM_STRING   "4"
-#define VERSION_REV_BIG_STRING     "6"
+#define VERSION_REV_BIG_STRING     "7"
 #define VERSION_REV_SMALL_STRING   "0"
 
 #define SHORT_NAME          "Tamale"
