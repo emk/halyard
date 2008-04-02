444c0ed27d80e9888cfe57dd535d6f4c96134700
Brian Campbell <brian.p.campbell@dartmouth.edu>
2008-04-02 19:53:54 +0000

    mizzen: Update fix-prologues.pl to understand Scheme files
    fix-prologues.pl is used to add copyright notices to the top of all
    source files.  This patch updates it to detect Scheme files, use
    appropriate comment delimiters in Scheme files, and apply the LGPL to
    them rather than the GPL.  It also detects whether it's operating on
    files in the mizzen directory, and uses "Mizzen" as the name of the
    program when in that directory.
    
    Furthermore, when we're in the Mizzen directory, we use 2006-2008 as
    the copyright years, the reason being that 2006 is the first time that
    any of the files in Mizzen were ever released.  We treat Halyard and
    Mizzen as distinct projects, so while the copyright years 1993-2008
    encompass everything distributed in Halyard, anything that is part
    of Mizzen has existed only as of 2006.
    
    Bug ID: 3939
    
    git-svn-id: https://imlsrc.dartmouth.edu/svn/public/halyard/trunk@29313 b24aca02-f418-0410-9c49-835e4aa915c3

--- tools/fix-prologues.pl
+++ tools/fix-prologues.pl
@@ -5,6 +5,17 @@
 # Fix license boilerplate and Emacs modelines in the specified files,
 # making backups.  On DOS, you may need to set PERLIO=crlf before running
 # this script.
+#
+# When updating the licenses (usually when updating the year), we should
+# probably run this script on the following files:
+# 
+#   find Common -name "*.cpp" -o -name "*.h" | xargs tools/fix-prologues.pl
+#   find wx/src -name "*.cpp" -o -name "*.h" | xargs tools/fix-prologues.pl
+#   find tools -name "*.cpp" -o -name "*.h" | xargs tools/fix-prologues.pl
+#   tools/fix-prologues.pl test/Runtime/halyard/*.ss
+#   tools/fix-prologues.pl test/Runtime/mizzen/*.ss
+
+use Cwd;
 
 #==========================================================================
 #  Configuration Options
@@ -14,10 +25,17 @@ my $modeline = <<'__EOD__';
 -*- Mode: C++; tab-width: 4; c-basic-offset: 4; -*-
 __EOD__
 
-my $copyright_and_license = <<'__EOD__';
+my $halyard_program_copyright = <<'__EOD__';
 Halyard - Multimedia authoring and playback system
 Copyright 1993-2008 Trustees of Dartmouth College
+__EOD__
 
+my $mizzen_program_copyright = <<'__EOD__';
+Mizzen - Scheme object system
+Copyright 2006-2008 Trustees of Dartmouth College
+__EOD__
+
+my $gpl_license = <<'__EOD__';
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
@@ -33,13 +51,59 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 __EOD__
 
+my $lgpl_license = <<'__EOD__';
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+__EOD__
+
 
 #==========================================================================
 #  Actual Code
 #==========================================================================
 
+my $cwd = cwd();
+
 # For each file on the command line.
 foreach my $filename (@ARGV) {
+    # Determine what kind of headers we should be adding
+    my $scheme_file = ($filename =~ m{\.ss$});
+    my $full_filename = "$cwd/$filename";
+    my $dirname = $full_filename;
+    # Extract the last directory in the path; if it's 'mizzen', we're
+    # in the mizzen project.
+    $dirname =~ s{^.*/([^/]+)/[^/]+$}{$1};
+    my $mizzen_file = ($dirname =~ /^mizzen$/);
+    
+    # Setup our comment characters and licenses
+    my $comment_prefix;
+    my $license;
+    my $program_copyright;
+    if ($scheme_file) {
+        $comment_prefix = ';;';
+        $license = $lgpl_license;
+    } else {
+        $comment_prefix = '//';
+        $license = $gpl_license;
+    }
+    if ($mizzen_file) {
+        $program_copyright = $mizzen_program_copyright;
+    } else {
+        $program_copyright = $halyard_program_copyright;
+    }
+    my $copyright_and_license = "$program_copyright\n$license";
+
     # If a backup file exists, delete it.
     unlink("$filename.bak");
 
@@ -56,38 +120,40 @@ foreach my $filename (@ARGV) {
     # Snarf the whole file into memory for easy processing.
     my @lines = <INPUT>;
 
-    # Remove any existing modeline.
-    if ($lines[0] =~ m{^// -\*- Mode:}) {
-        shift @lines;
+    if (!$scheme_file) {
+        # Remove any existing modeline.
+        if ($lines[0] =~ m{^// -\*- Mode:}) {
+            shift @lines;
+        }
+        
+        # Print a new modeline.
+        print OUTPUT "// $modeline";
     }
 
-    # Print a new modeline.
-    print OUTPUT "// $modeline";
-
     # Remove any existing license block.
-    if ($lines[0] =~ m{^// \@BEGIN_LICENSE}) {
+    if ($lines[0] =~ m{^$comment_prefix \@BEGIN_LICENSE}) {
         shift @lines;
-        while (!($lines[0] =~ m{^// \@END_LICENSE})) {
+        while (!($lines[0] =~ m{^$comment_prefix \@END_LICENSE})) {
             die "Can't find end of license block, stopping"
-                if (!($lines[0] =~ m{^//}));
+                if (!($lines[0] =~ m{^$comment_prefix}));
             shift @lines;
         }
         shift @lines;
     }
     
     # Print a new license block.
-    print OUTPUT <<'__EOD__';
-// @BEGIN_LICENSE
-//
+    print OUTPUT <<"__EOD__";
+$comment_prefix \@BEGIN_LICENSE
+$comment_prefix
 __EOD__
 
     foreach (split(/\n/, $copyright_and_license)) {
-        print OUTPUT "// $_\n";
+        print OUTPUT "$comment_prefix $_\n";
     }
     
-    print OUTPUT <<'__EOD__';
-//
-// @END_LICENSE
+    print OUTPUT <<"__EOD__";
+$comment_prefix
+$comment_prefix \@END_LICENSE
 __EOD__
 
     # Print a line of whitespace (if necessary).
