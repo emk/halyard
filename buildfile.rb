5a6ad4eea1efed11377d5aaa1b127f2cc1f920d6
Brian Campbell <brian.p.campbell@dartmouth.edu>
2008-04-08 15:41:26 -0400

    buildfile.rb: Add support for doing builds from Git
    Rework buildfile.rb to support Git, including the new work to break libs
    and media out separatly.  Specifically:
    
      * Clone our master Git repository, checkout the appropriate tag, and
        update submodules.
      * Build the following tarballs:
        - halyard-all-x.y.z.tar.gz - everything from halyard and submodules
        - halyard-x.y.z.tar.gz - everything but vendor libs and Media
        - halyard-libs-x.y.z.tar.gz - our versions of vendor
          libs
        - halyard-media-x.y.z.tar.gz - test/Media directory
        - halyard-test-x.y.z.zip - halyard/test, along with prebuilt binaries
        - mizzen-x.y.z.tar.gz - just mizzen
      * Removes the WXWIN environment variable, no longer needed
      * Everything is freshly added to Subversion tag, rather than copied
        in Subversion.
      * Adds checks for dirty_build? so it won't do Subversion tagging
        when testing.
      * Uploads files into a new directory hierarchy:
        - halyard-x.y.z/ - directory for platform-independent sources
          + halyard-all-x.y.z.tar.gz
          + halyard-x.y.z.tar.gz
          + halyard-libs-x.y.z.tar.gz
          + halyard-media-x.y.z.tar.gz
        - halyard-test/ - directory for halyard-test builds
          + halyard-test-x.y.z.zip
        - mizzen/ - directory for mizzen builds
          + mizzen-x.y.z.tar.gz

--- buildfile.rb
+++ buildfile.rb
@@ -3,6 +3,7 @@ include Buildscript
 require 'commands'
 
 svn_url = 'https://imlsrc.dartmouth.edu/svn'
+git_url = 'git://imlsrc.dartmouth.edu'
 release_dir = '//mccay/Mccay/Testing/Halyard'
 build_dir = 'c:/build/halyard'
 
@@ -11,14 +12,25 @@ unless version && /^[0-9.]*$/ =~ version
   raise "Error: bad version number: #{version}"
 end
 
-halyard_base_url = "#{svn_url}/public/halyard"
-tag_url = "#{halyard_base_url}/tags/#{version}"
+halyard_url = "#{git_url}/halyard"
 bin_url = "#{svn_url}/builds/halyard/#{version}"
 
 src_dir = "halyard-#{version}"
 test_dir = "halyard-test-#{version}"
-bin_dir = "halyard-bin-#{version}"
 mizzen_dir = "mizzen-#{version}"
+bin_dir = "halyard-bin-#{version}"
+
+all_archive = "halyard-all-#{version}.tar.gz"
+src_archive = "halyard-#{version}.tar.gz"
+libs_archive = "halyard-libs-#{version}.tar.gz"
+media_archive = "halyard-media-#{version}.tar.gz"
+mizzen_archive = "mizzen-#{version}.tar.gz"
+test_archive = "halyard-test-#{version}.zip"
+
+lib_dirs = %w(libs/boost libs/curl libs/freetype2 libs/libivorbisdec
+             libs/libxml2 libs/plt libs/portaudio libs/quake2
+             libs/sqlite libs/wxWidgets)
+media_dir = "test/Media"
 
 release_binaries = 
   %w(libmzgc2.dll libmzgc2_d.dll libmzsch2.dll libmzsch2_d.dll
@@ -28,45 +40,82 @@ release_binaries =
 web_host = 'iml.dartmouth.edu'
 web_ssh_user = 'iml_updater'
 web_path = '/var/www/halyard/dist'
+web_halyard_dir = "#{web_path}/halyard-#{version}"
+web_halyard_test_dir = "#{web_path}/halyard-test"
+web_mizzen_dir = "#{web_path}/mizzen"
 
 start_build(:build_dir => build_dir,
             :release_dir => release_dir,
             :signing_key => 'iml_authenticode_key')
 
 heading 'Check out the source code.', :name => :checkout do
-  svn :export, tag_url, src_dir
+  git :clone, halyard_url, src_dir
+  cd src_dir do |d|
+    git :checkout, "v#{version}"
+    git :submodule, 'init'
+    git :submodule, 'update'
+  end
 end
 
-heading 'Make source tarball.', :name => :source_tarball do
-  make_tarball src_dir
-  # Copy out clean copy of halyard/test before doing rake test
-  cp_r "#{src_dir}/test", test_dir
-  # Also, an independent distribution of mizzen
+# Build copies of all of our tarballs before we do any builds or testing,
+# so we will have clean trees.  make_tarball and make_tarball_from_files
+# will automatically ignore any .git and .gitignore files.
+heading 'Build source tarballs.', :name => :source_tarball do
+  # Build our halyard-all-x.y.z.tar.gz archive
+  make_tarball src_dir, :filename => all_archive
+
+  # Build our normal source archive, halyard-x.y.z.tar.gz, ecluding
+  # our libraries and media directory.
+  excludes = lib_dirs + [media_dir]
+  make_tarball src_dir, :filename => src_archive, :exclude => excludes
+
+  # Build our libs tarball.
+  lib_dirs_full = lib_dirs.map {|d| "#{src_dir}/#{d}"}
+  make_tarball_from_files lib_dirs_full, :filename => libs_archive
+
+  # Build our media tarball.
+  make_tarball "#{src_dir}/#{media_dir}", :filename => media_archive
+
+  # Build our mizzen tarball
   cp_r "#{src_dir}/test/Runtime/mizzen", mizzen_dir
+  make_tarball mizzen_dir, :filename => mizzen_archive
+
+  # Copy out clean copy of halyard/test before doing rake test; we will
+  # build the zipfile for this after building the engine and copying
+  # it in.
+  cp_r "#{src_dir}/test", test_dir
 end
 
 heading 'Building and testing engine.', :name => :build do
   cd src_dir do |d|
-    ENV['WXWIN'] = "#{build_dir}/#{src_dir}/libs/wxWidgets"
     run 'rake', 'test'
     # TODO - optionally sign the binaries
   end
 end
 
 heading 'Tagging Runtime and binaries in Subversion.', :name => :tag_binaries do
-  svn :mkdir, '-m', "Creating directory for release #{version}.", bin_url
-  svn :co, bin_url, "#{bin_dir}-svn"
-  cd "#{bin_dir}-svn" do |d|
-    svn :cp, "#{tag_url}/test/Runtime", "."
-    svn :cp, "#{tag_url}/test/Fonts", "."
-    svn :cp, "#{tag_url}/LICENSE.txt", "."
-    release_binaries.each do |file|
-      cp "#{build_dir}/#{src_dir}/Win32/Bin/#{file}", file
-      svn :add, file
-    end
-    svn :ci, '-m', "Tagging binaries for release #{version}."
+  if dirty_build?
+    mkdir "#{bin_dir}-svn"
+  else
+    svn :mkdir, '-m', "Creating directory for release #{version}.", bin_url
+    svn :co, bin_url, "#{bin_dir}-svn"
   end
-  svn :export, bin_url, bin_dir
+  cd "#{bin_dir}-svn" do |d|
+    full_src_dir = "#{build_dir}/#{src_dir}"
+
+    cp_r "#{full_src_dir}/test/Runtime", "."
+    svn :add, "Runtime" unless dirty_build?
+    cp_r "#{full_src_dir}/test/Fonts", "."
+    svn :add, "Fonts" unless dirty_build?
+    cp "#{full_src_dir}/LICENSE.txt", "."
+    svn :add, "LICENSE.txt" unless dirty_build?
+
+    release_binaries.each do |file|
+      cp "#{full_src_dir}/Win32/Bin/#{file}", file
+      svn :add, file unless dirty_build?
+    end
+    svn :ci, '-m', "Tagging binaries for release #{version}." unless dirty_build?
+  end
 end
 
 heading 'Releasing binaries to test project.', :name => :release_to_test do
@@ -76,23 +125,26 @@ heading 'Releasing binaries to test project.', :name => :release_to_test do
   cp "#{src_dir}/LICENSE.txt", test_dir
 end
 
-heading 'Building tarballs.', :name => :build_tarballs do
-  make_zipfile test_dir
-  make_tarball bin_dir
-  make_tarball mizzen_dir
+heading 'Building Halyard Test ZIP archive.', :name => :build_test_zip do
+  make_zipfile test_dir, :filename => test_archive
 end
 
 heading 'Uploading tarballs to website.', :name => :upload do
   server = remote_host(web_host, :user => web_ssh_user)
+
+  server.run 'mkdir', '-p', web_halyard_dir
+
   # TODO - we should base this on information in release_infos, but
   # that needs a bit of refactoring before it will be able to do what
   # we want.
-  [src_dir, bin_dir, mizzen_dir].each do |file|
-    # TODO - server.upload does rsync, while we only need scp. We might want
-    # to distinguish them.
-    server.upload "#{file}.tar.gz", web_path
+  # TODO - server.upload does rsync, while we only need scp. We might want
+  # to distinguish them.
+
+  [all_archive, src_archive, libs_archive, media_archive].each do |file|
+    server.upload file, web_halyard_dir
   end
-  server.upload "#{test_dir}.zip", web_path
+  server.upload test_archive, web_halyard_test_dir
+  server.upload mizzen_archive, web_mizzen_dir
   server.upload "#{src_dir}/Release-Notes.txt", web_path
 end
 
