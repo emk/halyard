emk/halyard

commit 7d97e7b789452de8568f2eef5152b74d2509aabb

wxMac: Compile DrawingArea
This was simply a matter of conditionalizing all the Quake-specific code
using CONFIG_HAVE_QUAKE2.  Unfortunately, this makes the code rather ugly.
We need to factor out these #if / #endif blocks into individual functions,
as per our standard coding style.  But to do that, I'll need to be running
on Windows machine.

wx/src/DrawingArea.cpp
@@ -25,7 +25,10 @@
 #include "DrawingAreaOpt.h"
 #include "Stage.h"
 #include "CommonWxConv.h"
+
+#if CONFIG_HAVE_QUAKE2
 #include "Quake2Engine.h"
+#endif // CONFIG_HAVE_QUAKE2
 
 using namespace Halyard;
 
@@ -64,9 +67,13 @@ void DrawingArea::InitializePixmap(bool inHasAlpha) {
 		mPixmap.UseAlpha();
 	Clear();
 
+#if CONFIG_HAVE_QUAKE2
     if (Quake2Engine::HaveInstance())
         InitializeQuake2Overlay();
+#endif // CONFIG_HAVE_QUAKE2
 }
+
+#if CONFIG_HAVE_QUAKE2
 
 void DrawingArea::InitializeQuake2Overlay()
 {
@@ -102,6 +109,8 @@ void DrawingArea::InitializeQuake2Overlay()
     mQuake2Overlay = ptr;
 }
 
+#endif // CONFIG_HAVE_QUAKE2
+
 void DrawingArea::InvalidateRect(const wxRect &inRect, int inInflate,
                                  bool inHasPixmapChanged)
 {
@@ -113,8 +122,10 @@ void DrawingArea::InvalidateRect(const wxRect &inRect, int inInflate,
     r.Intersect(wxRect(wxPoint(0, 0),
                        mBounds.GetSize()));
 	if (!r.IsEmpty()) {
+#if CONFIG_HAVE_QUAKE2
         if (inHasPixmapChanged && mQuake2Overlay)
             mQuake2Overlay->DirtyRect(r);
+#endif // CONFIG_HAVE_QUAKE2
 	    r.Offset(mBounds.GetPosition());
 	    mStage->InvalidateRect(r);
 	}
@@ -126,9 +137,11 @@ void DrawingArea::InvalidateDrawingArea(bool inHasPixmapChanged) {
 }
 
 void DrawingArea::SetSize(const wxSize &inSize) {
+#if CONFIG_HAVE_QUAKE2
     // If we have a Quake 2 overlay, get rid of it.
     if (mQuake2Overlay)
         mQuake2Overlay = shared_ptr<wxQuake2Overlay>();
+#endif // CONFIG_HAVE_QUAKE2
 
     // Invalidate the rectangle covered by our original size.
     InvalidateDrawingArea(false);
@@ -147,8 +160,10 @@ void DrawingArea::SetSize(const wxSize &inSize) {
 void DrawingArea::Show(bool inShow) {
     if (inShow != mIsShown) {
         mIsShown = inShow;
+#if CONFIG_HAVE_QUAKE2
         if (mQuake2Overlay)
             mQuake2Overlay->Show(inShow);
+#endif // CONFIG_HAVE_QUAKE2
         InvalidateDrawingArea(false);
     }
 }
@@ -156,8 +171,10 @@ void DrawingArea::Show(bool inShow) {
 void DrawingArea::MoveTo(const wxPoint &inPoint) {
     InvalidateDrawingArea(false);
     mBounds = wxRect(inPoint, mBounds.GetSize());
+#if CONFIG_HAVE_QUAKE2
     if (mQuake2Overlay)
         mQuake2Overlay->MoveTo(inPoint);
+#endif // CONFIG_HAVE_QUAKE2
     InvalidateDrawingArea(false);
 }
 

wx/src/DrawingArea.h
@@ -24,7 +24,10 @@
 #define DrawingArea_H
 
 class Stage;
+
+#if CONFIG_HAVE_QUAKE2
 class wxQuake2Overlay;
+#endif // CONFIG_HAVE_QUAKE2
 
 /// An object which can be drawn to by scripts.
 class DrawingArea : public GraphicsTools::Image {
@@ -32,7 +35,10 @@ class DrawingArea : public GraphicsTools::Image {
 	wxRect mBounds;
     wxBitmap mPixmap;
     bool mIsShown;
+
+#if CONFIG_HAVE_QUAKE2
     shared_ptr<wxQuake2Overlay> mQuake2Overlay;
+#endif // CONFIG_HAVE_QUAKE2
 
     //////////
     /// Returns true if this DrawingArea has an area of zero.
@@ -44,12 +50,14 @@ class DrawingArea : public GraphicsTools::Image {
     ///
 	void InitializePixmap(bool inHasAlpha);
 
+#if CONFIG_HAVE_QUAKE2
     ///////////
     /// Initialize the Quake 2 overlay object associated with this drawing
     /// area.  We'll only call this function if we are floating this
     /// DrawingArea over Quake 2.
     ///
     void InitializeQuake2Overlay();
+#endif // CONFIG_HAVE_QUAKE2
 
 	//////////
 	/// Invalidate the specified rectangle.

wx/src/Makefile.am
@@ -36,8 +36,8 @@ libhalyardgui_a_SOURCES = \
 	dlg/StartupDlg.cpp dlg/StartupDlg.h \
 	dlg/XrcDlg.cpp dlg/XrcDlg.h \
 	DocNotebook.cpp DocNotebook.h \
-	Downloader.cpp Downloader.h
-#	DrawingArea.cpp DrawingArea.h \
+	Downloader.cpp Downloader.h \
+	DrawingArea.cpp DrawingArea.h
 #	DrawingAreaOpt.cpp DrawingAreaOpt.h \
 #	DrawingContextStack.cpp DrawingContextStack.h \
 #	EditBox.cpp EditBox.h \

commit 4ef92cecc029925955509a9583c5e7ba84a0dab9

DrawingArea: Move conditional code into top-level functions
[Portability fix: Make sure we actually include "AppConfig.h".  Without it,
we had Quake 2 support, but no overlays.  This problem was accidentally
introduced in 7d97e7b789452de8568f2eef5152b74d2509aabb.]

This code now complies with our coding standards for conditional
compilation: All the conditional snippets have been moved to their own
top-level functions, which are wrapped in a single #if / #else / #endif.
We provide stub implementations for platforms without Quake 2.

wx/src/DrawingArea.cpp
@@ -67,16 +67,16 @@ void DrawingArea::InitializePixmap(bool inHasAlpha) {
 		mPixmap.UseAlpha();
 	Clear();
 
-#if CONFIG_HAVE_QUAKE2
-    if (Quake2Engine::HaveInstance())
-        InitializeQuake2Overlay();
-#endif // CONFIG_HAVE_QUAKE2
+    MaybeInitializeGameOverlay();
 }
 
 #if CONFIG_HAVE_QUAKE2
 
-void DrawingArea::InitializeQuake2Overlay()
-{
+void DrawingArea::MaybeInitializeGameOverlay() {
+    // If we don't have a running Quake 2 instance, return immediately.
+    if (!Quake2Engine::HaveInstance())
+        return;
+
     // If the pixmap has area zero, then we don't have any valid data to
     // pass to Quake anyway.
     if (HasAreaOfZero())
@@ -109,7 +109,39 @@ void DrawingArea::InitializeQuake2Overlay()
     mQuake2Overlay = ptr;
 }
 
-#endif // CONFIG_HAVE_QUAKE2
+void DrawingArea::GameOverlayDelete() {
+    if (mQuake2Overlay)
+        mQuake2Overlay = shared_ptr<wxQuake2Overlay>();
+}
+
+void DrawingArea::GameOverlayDirtyRect(const wxRect &inRect) {
+    if (mQuake2Overlay)
+        mQuake2Overlay->DirtyRect(inRect);
+}
+
+void DrawingArea::GameOverlayShow(bool inShouldShow) {
+    if (mQuake2Overlay)
+        mQuake2Overlay->Show(inShouldShow);
+}
+
+void DrawingArea::GameOverlayMoveTo(const wxPoint &inPoint) {
+    if (mQuake2Overlay)
+        mQuake2Overlay->MoveTo(inPoint);
+}
+
+#else // !CONFIG_HAVE_QUAKE2
+
+void DrawingArea::MaybeInitializeGameOverlay() {}
+
+void DrawingArea::GameOverlayDelete() {}
+
+void DrawingArea::GameOverlayDirtyRect(const wxRect &inRect) {}
+
+void DrawingArea::GameOverlayShow(bool inShouldShow) {}
+
+void DrawingArea::GameOverlayMoveTo(const wxPoint &inPoint) {}
+
+#endif // !CONFIG_HAVE_QUAKE2
 
 void DrawingArea::InvalidateRect(const wxRect &inRect, int inInflate,
                                  bool inHasPixmapChanged)
@@ -122,10 +154,8 @@ void DrawingArea::InvalidateRect(const wxRect &inRect, int inInflate,
     r.Intersect(wxRect(wxPoint(0, 0),
                        mBounds.GetSize()));
 	if (!r.IsEmpty()) {
-#if CONFIG_HAVE_QUAKE2
-        if (inHasPixmapChanged && mQuake2Overlay)
-            mQuake2Overlay->DirtyRect(r);
-#endif // CONFIG_HAVE_QUAKE2
+        if (inHasPixmapChanged)
+            GameOverlayDirtyRect(r);
 	    r.Offset(mBounds.GetPosition());
 	    mStage->InvalidateRect(r);
 	}
@@ -137,11 +167,8 @@ void DrawingArea::InvalidateDrawingArea(bool inHasPixmapChanged) {
 }
 
 void DrawingArea::SetSize(const wxSize &inSize) {
-#if CONFIG_HAVE_QUAKE2
     // If we have a Quake 2 overlay, get rid of it.
-    if (mQuake2Overlay)
-        mQuake2Overlay = shared_ptr<wxQuake2Overlay>();
-#endif // CONFIG_HAVE_QUAKE2
+    GameOverlayDelete();
 
     // Invalidate the rectangle covered by our original size.
     InvalidateDrawingArea(false);
@@ -160,10 +187,7 @@ void DrawingArea::SetSize(const wxSize &inSize) {
 void DrawingArea::Show(bool inShow) {
     if (inShow != mIsShown) {
         mIsShown = inShow;
-#if CONFIG_HAVE_QUAKE2
-        if (mQuake2Overlay)
-            mQuake2Overlay->Show(inShow);
-#endif // CONFIG_HAVE_QUAKE2
+        GameOverlayShow(inShow);
         InvalidateDrawingArea(false);
     }
 }
@@ -171,10 +195,7 @@ void DrawingArea::Show(bool inShow) {
 void DrawingArea::MoveTo(const wxPoint &inPoint) {
     InvalidateDrawingArea(false);
     mBounds = wxRect(inPoint, mBounds.GetSize());
-#if CONFIG_HAVE_QUAKE2
-    if (mQuake2Overlay)
-        mQuake2Overlay->MoveTo(inPoint);
-#endif // CONFIG_HAVE_QUAKE2
+    GameOverlayMoveTo(inPoint);
     InvalidateDrawingArea(false);
 }
 

wx/src/DrawingArea.h
@@ -23,6 +23,8 @@
 #ifndef DrawingArea_H
 #define DrawingArea_H
 
+#include "AppConfig.h"
+
 class Stage;
 
 #if CONFIG_HAVE_QUAKE2
@@ -50,14 +52,21 @@ class DrawingArea : public GraphicsTools::Image {
     ///
 	void InitializePixmap(bool inHasAlpha);
 
-#if CONFIG_HAVE_QUAKE2
-    ///////////
-    /// Initialize the Quake 2 overlay object associated with this drawing
-    /// area.  We'll only call this function if we are floating this
-    /// DrawingArea over Quake 2.
-    ///
-    void InitializeQuake2Overlay();
-#endif // CONFIG_HAVE_QUAKE2
+    /// If we have a copy wxQuake2, and it's running, then initialize
+    /// the Quake 2 overlay object associated with this drawing area.
+    void MaybeInitializeGameOverlay();
+
+    /// Delete any game overlay associated with this drawing area.
+    void GameOverlayDelete();
+
+    /// If we have a game overlay, mark part of it as dirty.
+    void GameOverlayDirtyRect(const wxRect &inRect);
+
+    /// If we have a game overlay, show or hide it.
+    void GameOverlayShow(bool inShouldShow);
+
+    /// If we have a game overlay, move it to the specified location.
+    void GameOverlayMoveTo(const wxPoint &inPoint);
 
 	//////////
 	/// Invalidate the specified rectangle.