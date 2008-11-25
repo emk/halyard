72de2cc692481b6d1a792f32c276a4bdec06c045
Eric Kidd <eric.m.kidd@dartmouth.edu>
2008-11-25 15:48:58 -0500

    0.5.22 - 25 Nov 2008
    We've finished overhauling the server-side portion of the crash reporter,
    and we need some more changes on the client side.  This release includes
    both fixes to the crash reporter infrastructure, and workarounds for
    various issues discovered by the crash reporter.
    
    Eric Kidd (6):
          Add crash report URL to halyard/test
          Force QuickTime to run in Safe/GDI mode
          Include full text of PortAudio error messages
          Crash reporter: Search for curl executable in script and runtime dirs
          Add %force-crash function
          Crash reporter: Display error if we can't create temp dir

--- Common/TVersion.h
+++ Common/TVersion.h
@@ -22,14 +22,14 @@
 
 #define VERSION_MAJOR_NUM          0
 #define VERSION_MINOR_NUM          5
-#define VERSION_REV_BIG            21
+#define VERSION_REV_BIG            22
 #define VERSION_REV_SMALL          0
 
 // For various unfortunate preprocessor reasons, we need a duplicate copy
 // in string format.
 #define VERSION_MAJOR_NUM_STRING   "0"
 #define VERSION_MINOR_NUM_STRING   "5"
-#define VERSION_REV_BIG_STRING     "21"
+#define VERSION_REV_BIG_STRING     "22"
 #define VERSION_REV_SMALL_STRING   "0"
 
 // This string may optionally contain something like "-pre1" or "-rc2".
