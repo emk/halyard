63c18356c5724af8c07f54b80c0ef9e6d005c847
Brian Campbell <brian.p.campbell@dartmouth.edu>
2009-09-30 18:58:02 -0400

    Updater: Add test coverage for smarter update installer
    When I added the features to the update installer to allow it to copy
    or move files from the existing installation, I hadn't written any
    tests specifically for that functionality; I had relied on the
    existing unit tests while simply deleting one redundant file from the
    pool that already existed elsewhere on disk.
    
    This patch adds much more complete test coverage for the new smart
    updtate installer, exercising it's ability to deal with files copied or
    moved from files already installed, files overwritten by newly downloaded
    files, and files that are installed in multiple locations by being
    copied until the last destination, at which point they are moved.
    
    This patch also introduces FixtureBuilder, which is a micro DSL for
    creating a directory tree and populating with files with given contents.
    It also has methods for generating manifests and release.spec files,
    to make generating fixtures for the updater easier.  This allows for
    much easier generation of new test cases than the previous slow,
    error prone method of generating all of the fixture directories by
    hand.

--- tools/UpdateInstaller/update_installer_test.rb
+++ tools/UpdateInstaller/update_installer_test.rb
@@ -24,6 +24,9 @@ require 'test/unit'
 require 'fileutils'
 require 'pathname'
 
+$foo_digest = "855426068ee8939df6bce2c2c4b1e7346532a133"
+$null_digest = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
 class UpdateInstallerTest < Test::Unit::TestCase
   class << self
     attr_accessor :fixture_dir
@@ -84,7 +87,7 @@ class UpdateInstallerSimpleTest < UpdateInstallerTest
 
   def test_install
     download_mtime = 
-      File.mtime("Updates/pool/855426068ee8939df6bce2c2c4b1e7346532a133")
+      File.mtime("Updates/pool/#{$foo_digest}")
     sleep 2
     assert run_exe(".", ".")
     assert !File.exists?("UPDATE.LCK")
@@ -117,7 +120,7 @@ EOF
   end
 
   def test_failed_install
-    FileUtils.rm "Updates/pool/855426068ee8939df6bce2c2c4b1e7346532a133"
+    FileUtils.rm "Updates/pool/#{$foo_digest}"
     assert_fails_gracefully
   end
 
@@ -141,3 +144,148 @@ EOF
   #end
 end
 
+class FixtureBuilder
+  def initialize
+    @saved = { }
+  end
+
+  def dir name, &block
+    FileUtils.mkdir_p name
+    if block_given?
+      FileUtils.cd name do |d|
+        yield self
+      end
+    end
+    name
+  end
+
+  def file name, contents
+    File.open(name, "w") { |f| f << contents }
+    name
+  end
+
+  def pool_file contents
+    name = Digest::SHA1.hexdigest(contents)
+    file name, contents
+  end
+
+  def copy source, dest
+    FileUtils.cp_r source, dest
+    dest
+  end
+
+  def build_manifest component, files
+    name = "MANIFEST.#{component}"
+    File.open(name, "w") do |f|
+      files.each do |path|
+        f << manifest_entry(path)
+      end
+    end
+    name
+  end
+
+  def build_release_spec build_id
+    name = "release.spec"
+    File.open(name, "w") do |f|
+      f << "Build: " << build_id << "\n\n"
+      manifests = Dir["MANIFEST.*"]
+      manifests.each do |manifest|
+        f << manifest_entry(manifest)
+      end
+    end
+    name
+  end
+
+  def []=(key, name)
+    @saved[key] = Dir.getwd + "/" + name
+  end
+  
+  def [](key)
+    @saved[key]
+  end
+
+  private
+
+  def manifest_entry path
+    digest = Digest::SHA1.hexdigest(IO.read(path))
+    size = File.size(path)
+    "#{digest} #{size} #{path}\n"
+  end
+end
+
+class UpdateInstallerSmarterUpdateTest < UpdateInstallerTest
+  COPIED_TEXT = "This file should be copied"
+  MOVED_TEXT = "This file should be moved"
+  OVERWRITTEN_TEXT = "Here are the new contents of this file"
+  MULTIPLE_LOCATIONS_TEXT = "This file should be installed in multiple locations"
+
+  # Override the superclass that uses an existing fixture dir, and
+  # simply generate the fixture dir that we need.
+  def setup
+    FileUtils.rm_rf "fixture-temp"
+    FixtureBuilder.new.dir "fixture-temp" do |fb|
+      fb.dir "installed-program" do |fb|
+        fb.file "copied", COPIED_TEXT
+        fb.file "deleted", "This file should be deleted"
+        fb.file "moved", MOVED_TEXT
+        fb.file "overwritten", "This file should be overwritten"
+        fb.build_manifest("component", %w[copied deleted moved overwritten])
+        fb.build_release_spec "build-A"
+      end
+      fb.dir "new-version" do |fb|
+        fb.file "copied", COPIED_TEXT
+        fb.file "from-copied", COPIED_TEXT
+        fb.file "from-moved", MOVED_TEXT
+        fb.file "overwritten", OVERWRITTEN_TEXT
+        %w[1 2 3].each do |i|
+          fb.file "multiple-locations-#{i}", MULTIPLE_LOCATIONS_TEXT
+        end
+        fb["manifest"] = fb.build_manifest("component", 
+                                           %w[copied from-copied from-moved
+                                              overwritten multiple-locations-1
+                                              multiple-locations-2
+                                              multiple-locations-3])
+        fb["release.spec"] = fb.build_release_spec "build-B"
+      end
+      fb.dir "download-dir/Updates" do |fb|
+        fb.copy fb["release.spec"], "release.spec"
+        fb.dir "pool" do |fb|
+          fb.pool_file OVERWRITTEN_TEXT
+          fb.pool_file MULTIPLE_LOCATIONS_TEXT
+        end
+        fb.dir "manifests/build-B" do |fb|
+          fb.copy fb["manifest"], "MANIFEST.component"
+        end
+      end
+    end
+    FileUtils.cd "fixture-temp"
+
+    run_exe "download-dir", "installed-program"
+  end
+
+  def test_copy_files_from_tree
+    assert_file_equals COPIED_TEXT, "installed-program/copied"
+    assert_file_equals COPIED_TEXT, "installed-program/from-copied"
+  end
+
+  def test_move_files_in_tree
+    assert !File.exists?("installed-program/moved")
+    assert_file_equals MOVED_TEXT, "installed-program/from-moved"
+  end
+
+  def test_delete_files_from_tree
+    assert !File.exists?("installed-program/deleted")
+  end
+
+  def test_overwrite_file
+    assert_file_equals OVERWRITTEN_TEXT, "installed-program/overwritten"
+  end
+
+  def test_pool_cleared
+    %w[1 2 3].each do |i|
+      assert_file_equals(MULTIPLE_LOCATIONS_TEXT, 
+                         "installed-program/multiple-locations-#{i}")
+    end
+    assert_equal [], Dir["download-dir/Updates/pool/*"]
+  end
+end
