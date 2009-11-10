72d57d70f2888b1142418af61ad52068d65223ff
Eric Kidd <eric.m.kidd@dartmouth.edu>
2009-11-10 12:19:37 -0500

    0.6.0 - 9 Nov 2009
    These release notes only cover the difference between 0.5.43 and 0.6.0, and
    not the cumulative differences between 0.4.* and 0.6.0.  This release
    includes a build fix for the UpdateInstaller test suites, several tweaks to
    the HACP API to make it more convenient, and support for loading more icon
    sizes.
    
    Brian Campbell (1):
          UpdateInstaller: Require 'digest/sha1' in tests
    
    Eric Kidd (3):
          HACP: Allow hacp-write to be called from a group, and tweak API
          hacp: Call current-group-member, not current-card in hacp-write
          Add support for more Windows icon sizes

--- Common/TVersion.h
+++ Common/TVersion.h
@@ -21,15 +21,15 @@
 // @END_LICENSE
 
 #define VERSION_MAJOR_NUM          0
-#define VERSION_MINOR_NUM          5
-#define VERSION_REV_BIG            43
+#define VERSION_MINOR_NUM          6
+#define VERSION_REV_BIG            0
 #define VERSION_REV_SMALL          0
 
 // For various unfortunate preprocessor reasons, we need a duplicate copy
 // in string format.
 #define VERSION_MAJOR_NUM_STRING   "0"
-#define VERSION_MINOR_NUM_STRING   "5"
-#define VERSION_REV_BIG_STRING     "43"
+#define VERSION_MINOR_NUM_STRING   "6"
+#define VERSION_REV_BIG_STRING     "0"
 #define VERSION_REV_SMALL_STRING   "0"
 
 // This string may optionally contain something like "-pre1" or "-rc2".
@@ -40,7 +40,7 @@
 #define VERSION_STRING \
     SHORT_NAME " " VERSION_MAJOR_NUM_STRING "." \
     VERSION_MINOR_NUM_STRING "." VERSION_REV_BIG_STRING "." \
-    VERSION_REV_SMALL_STRING VERSION_EXTRA " (Development)"
+    VERSION_REV_SMALL_STRING VERSION_EXTRA ""
 
 // Needed for Windows resource files.
 #define VERSION_COMMAS \
